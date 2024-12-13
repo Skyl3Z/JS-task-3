77625ebd2464aad3af787e59f57d1cf6f1974438
Skyl3Z <sabagogichaishvili15@gmail.com>
2024-12-13 21:56:31 +0400

    third js task

---
+++ index.js
@@ -0,0 +1,38 @@
+//task 1
+
+// let score = 95 ;
+
+// switch (true) {
+
+//     case (score === 100):
+//         console.log("A+".valueOf());
+//         break;
+
+//     case (score >= 90 && score < 100):
+//         console.log(typeof "A");
+//         break;
+
+//     case (score >= 80 && score < 90):
+//         console.log(typeof "B");
+//         break;
+
+//     case (score >= 60 && score < 80):
+//         console.log(typeof "C");
+//         break;
+
+//     case (score < 60):
+//         console.log(typeof "F".valueOf());
+//         break;
+
+//     default:
+//         console.log(typeof "Invalid score");
+//         break;
+// }
+
+//task 2
+
+let string = "123213" ;
+let number = Number(string) ;
+
+console.log(typeof number) ;
+
